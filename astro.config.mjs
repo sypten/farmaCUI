79e0658c4ca07aef088bc11a9281a73bada6ba06
sypten <sypten.co@gmail.com>
2025-12-17 17:50:20 -0300

    Switch to static output for standard HTML build

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  integrations: [tailwindcss(), react()],
-  adapter: vercel({
-    edgeMiddleware: false
-  })
+  output: 'static',
+  integrations: [tailwindcss(), react()]
 });
